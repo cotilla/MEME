bea3096d0f2117ff3e4c4aba5c60b876fe860b83
cotilla <matthew.cotilla@gmail.com>
2018-10-09 10:21:56 -0400

    Initial commit
    Meme proj

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var duck;  
+
+function preload() {
+   duck = loadImage("killer_duck.jpg"); 
+}
+
+
+function setup() {
+	createCanvas(1048, 1032);
+}
+
+function draw() {
+	background(0);
+  image(duck, 0, 0, width, height);  
+
+	
+	
+	
+	
+	}
+	
+	
